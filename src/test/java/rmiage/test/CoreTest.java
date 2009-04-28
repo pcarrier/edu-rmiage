e865858124a33173ec80d6a40dd181a014a8b013
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-04-28 16:04:02 +0200

    Added empty test suite for framework.core...

---
+++ src/test/java/rmiage/test/CoreTest.java
@@ -0,0 +1,37 @@
+package rmiage.test;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CoreTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CoreTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
