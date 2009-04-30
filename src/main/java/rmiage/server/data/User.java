0e6d4d5d071e284baf0110fe831309c929d7198a
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-04-30 13:16:20 +0200

    Classes User et UserGroup de base.

---
+++ src/main/java/rmiage/server/data/User.java
@@ -0,0 +1,18 @@
+package rmiage.server.data;
+
+import rmiage.framework.data.Content;
+import rmiage.framework.data.Searchable;
+
+public class User extends Content implements Searchable{
+
+	public boolean contains(String searched) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean matches(String searched) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/rmiage/server/data/UserGroup.java
@@ -0,0 +1,18 @@
+package rmiage.server.data;
+
+import rmiage.framework.data.Container;
+import rmiage.framework.data.Searchable;
+
+public class UserGroup extends Container implements Searchable{
+
+	public boolean contains(String searched) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean matches(String searched) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
