89b9393ab38a483ae2a6d9db322ae50c64f77552
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-04-30 14:50:55 +0200

    Utilisation de la genericite.

--- src/main/java/rmiage/framework/data/Container.java
+++ src/main/java/rmiage/framework/data/Container.java
@@ -10,10 +10,10 @@ import java.util.ArrayList;
  *
  * @author jc
  */
-public class Container extends Content {
+public class Container<T extends Content> extends Content {
         
     //List of Contents Children
-    private ArrayList<Content> ContentChildren;
+    private ArrayList<T> ContentChildren;
 
     /**
      * Build a Container with he's string representation
@@ -21,7 +21,7 @@ public class Container extends Content {
     //test ok
     public Container() {
         super();
-        this.ContentChildren = new ArrayList<Content>();
+        this.ContentChildren = new ArrayList<T>();
     }
 
     /**
@@ -29,7 +29,7 @@ public class Container extends Content {
      * @return an ArrayList<Content> containing all first order Content Children
      */
     //tested ok
-    public ArrayList<Content> Contents() {
+    public ArrayList<T> Contents() {
         return this.ContentChildren;
     }
 
@@ -39,11 +39,12 @@ public class Container extends Content {
      * @param f the Content to add
      */
    //tested ok
-    public void addContent(Content f) {
+    public void addContent(T f) {
     	if(f!=null){
         if (!this.Contents().contains(f)) {
         	f.addParent(this);
-            this.Contents().add(f);
+            this.Contents().add((T)f);
+            
         }
     	}
     }
@@ -53,9 +54,10 @@ public class Container extends Content {
      * Drop all first order Contents.
      */
     //tested ok
-    public void dropContents(){
-    	ArrayList<Content> tmp = (ArrayList<Content> ) this.Contents().clone();
-    	for(Content f:tmp){
+    @SuppressWarnings("unchecked")
+	public void dropContents(){
+    	ArrayList<T> tmp = (ArrayList<T> ) this.Contents().clone();
+    	for(T f:tmp){
     		this.dropContent(f);
     	}
     }
@@ -64,7 +66,7 @@ public class Container extends Content {
      * @param f the Content to drop.
      */
     //tested ok
-    public void dropContent(Content f){
+    public void dropContent(T f){
     	if (this.Contents().contains(f)){
     		this.Contents().remove(f);
     		f.dropParent(this);
