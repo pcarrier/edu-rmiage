24e7b7a0de6d64de89858042dde1d0922286b87b
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-05-01 21:07:36 +0200

    Nos user sont searchable

--- src/main/java/rmiage/server/data/User.java
+++ src/main/java/rmiage/server/data/User.java
@@ -1,34 +1,48 @@
 package rmiage.server.data;
 
 
+
+import rmiage.framework.data.Container;
+import rmiage.framework.data.Conversation;
 import rmiage.framework.data.Searchable;
 import rmiage.framework.data.UserBasic;
 
 public class User extends UserBasic implements Searchable{
-
-	@SuppressWarnings("unused")
-	private String pass;
 	
+	protected String password;
+	protected Container<Conversation> conversations;
+	
+	@SuppressWarnings("unchecked")
 	public User(String login, String pass){
 		super(login);
 		this.setPass(pass);
+		this.conversations=new Container<Conversation>();
 	}
 
+	public void delete(){
+		this.conversations.delete();
+		super.delete();
+	}
 	public String getPass(){
 		return this.getPass();
 	}
 	
 	public void setPass(String pass) {
-		this.pass = pass;
+		this.password = pass;
 	}
 	
+	public Container<Conversation> getConversations(){
+		return this.conversations;
+	}
+	
+	public boolean addConversation(Conversation c){
+		return this.conversations.addContent(c);
+	}
 	public boolean contains(String searched) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.login.toLowerCase().contains(searched);
 	}
 
 	public boolean matches(String searched) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.login.toLowerCase().equals(searched);
 	}
 }
