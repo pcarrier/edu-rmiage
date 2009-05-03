2f9cdd1e9f5fa1c113e7f635436822e2e510de49
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-05-03 23:57:46 +0200

    Hop hop hop un StandartLoginController expose en rmi et instancie dans le controller principal.

---
+++ src/main/java/rmiage/server/connection/ConnectionManager.java
@@ -0,0 +1,49 @@
+package rmiage.server.connection;
+
+import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+
+public class  ConnectionManager {
+
+	protected Registry registry;
+	
+	
+	public ConnectionManager(int port) throws RemoteException{
+		registry = java.rmi.registry.LocateRegistry.createRegistry(port);
+	}
+	
+	public void stop(){
+		if (this.registry != null) {
+			try {
+				//Unbind each bounded uri.
+				for (String bound : this.registry.list()) {
+						this.registry.unbind(bound);
+				}
+			} catch (Exception e) {
+				System.out.println("An error occured when stopping.");
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Rmiregistry Stoped");
+	}
+	
+	public void bind(String uri, Remote obj){
+		if (this.registry != null) {
+			try {
+				this.registry.bind(uri, obj);
+			} catch (AccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (AlreadyBoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
