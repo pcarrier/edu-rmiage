630d1e33b7a14dbdd85cb82e2dbaa7777cac6785
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2009-05-04 01:27:20 +0200

    Petites modifs mineures sur MainController (code mort/inutile)

--- src/main/java/rmiage/server/controller/MainController.java
+++ src/main/java/rmiage/server/controller/MainController.java
@@ -6,77 +6,43 @@ import rmiage.server.connection.ConnectionManager;
 import rmiage.server.settings.SettingsException;
 import rmiage.server.settings.SettingsController;
 
-
 public class MainController {
 
-	protected SettingsController settingsController;
-	protected ConnectionManager connection;
+    protected SettingsController settingsController;
+    protected ConnectionManager connectionManager;
 
-	@SuppressWarnings("unchecked")
-	private SettingsController getSettingsController() {
-		SettingsController res = null;
-		try {
-			String settingsControllerClassName = System.getProperty(
-					"rmiage.settingsloader", "rmiage.framework");
-			Class SettingsControllerClass = Class.forName(
-					settingsControllerClassName).getClass();
-			res = (SettingsController) SettingsControllerClass.newInstance();
-		} catch (InstantiationException ex) {
-			throw new SettingsException(
-					"Cannot instantiate the settings controller");
-		} catch (IllegalAccessException ex) {
-			throw new SettingsException(
-					"Illegal access around the settings controller");
-		} catch (ClassNotFoundException ex) {
-			throw new SettingsException(
-					"Settings controller class cannot be found!");
-		}
-		
-		return res;
-	}
+    public MainController(String[] args) throws RemoteException {
+        init(args);
+    }
 
-	/**
-	 * Initialize the server with command line args.
-	 * 
-	 * @param cmdLineParams
-	 *            params from command line.
-	 * @throws RemoteException 
-	 */
-	public void init(String[] cmdLineParams) throws InterruptedException, RemoteException {
-		
-		this.settingsController = getSettingsController();
-		int rmiport = this.settingsController.getRmiPort();
+    /**
+     * Initialize the server with command line args.
+     *
+     * @param cmdLineParams
+     *            params from command line.
+     * @throws RemoteException
+     */
+    public void init(String[] cmdLineParams) throws ConnectionException {
+        settingsController = new SettingsController(cmdLineParams);
+        int RMIPort = settingsController.getRmiPort();
+        connectionManager = new ConnectionManager(RMIPort);
+        connectionManager.bind("rmi://127.0.0.1/login", new StandardLoginController());
+    }
 
-		if (this.settingsController != null) {
-			
-			this.connection = new ConnectionManager(rmiport);
-			this.connection.bind("rmi://127.0.0.1/login", new StandardLoginController());
-		}
-	}
+    public static void main(String[] args) {
+        MainController controller = null;
+        try {
+            controller = new MainController(args);
+        } catch (SettingsException ex) {
+            System.err.println(ex.getMessage());
+        } catch (ConnectionException ex) {
+            System.err.println(ex.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-
-	public static void main(String[] args){
-		MainController controller = new MainController();
-		try {
-			controller.init(args);
-			
-		} catch (InterruptedException e) {
-			System.out.println("Received ^C");
-			controller.connection.stop();
-		} catch (SettingsException e) {
-			System.out.println(e.getMessage());
-			System.out.println("Server can't start.");
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-		
-	}
-	public void finalize(){
-		System.out.println("Server stopped");
-	}
-	
+    public void finalize() {
+        System.out.println("Server stopped");
+    }
 }
