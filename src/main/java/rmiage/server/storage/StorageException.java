f35fea246b8b13368cf8b1283ed77b7a34ac4678
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2009-05-03 20:54:44 +0200

    DefaultSettingsController just sucks.

---
+++ src/main/java/rmiage/server/storage/StorageException.java
@@ -0,0 +1,17 @@
+package rmiage.server.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String msg) {
+        super(msg);
+    }
+
+    @Override
+    public String toString() {
+        if (getMessage().isEmpty()) {
+            return ("Error within storage!");
+        } else {
+            return ("Error with storage: " + getMessage() + "!");
+        }
+    }
+}
--- src/main/java/rmiage/server/storage/StorageManager.java
+++ src/main/java/rmiage/server/storage/StorageManager.java
@@ -1,22 +1,35 @@
 package rmiage.server.storage;
 
-import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class StorageManager {
 
-    protected ArrayList<BackendAssociation> backAss;
+    protected Hashtable<String, IBackend> backAss = new Hashtable<String, IBackend>();
 
-    public void associateBackend(String str, IBackend bm) {
-        //TODO
+    public void associateBackend(String id, IBackend backend) {
+        backAss.put(id, backend);
     }
 
-    public void detachBackend(String str) {
-        //TODO
+    public void detachBackend(String id) {
+        backAss.remove(id);
     }
 
-    public BackendAssociation[] getBackendAssociation() {
-        return (BackendAssociation[]) backAss.toArray();
-    }
+    public StorageManager(Hashtable<String, String> backendDescrs) {
+        for (String backId : backendDescrs.values()) {
+            IBackend backend;
+            Class backendClass;
+            try {
+                backendClass = Class.forName(backendDescrs.get(backId)).getClass();
+                backend = (IBackend) backendClass.newInstance();
+                backAss.put(backId, backend);
+            } catch (InstantiationException ex) {
+                throw new StorageException("Cannot instantiate the backend: " + backId);
+            } catch (IllegalAccessException ex) {
+                throw new StorageException("Illegal access around the backend: " + backId);
+            } catch (ClassNotFoundException ex) {
+                throw new StorageException("The class cannot be found for the backend: " + backId);
+            }
 
-    
+        }
+    }
 }
