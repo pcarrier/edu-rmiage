8596f759d295a80197119a1a20cd4fca66ab1371
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-05-03 20:48:51 +0200

    L'interface de backend.

--- src/main/java/rmiage/server/storage/IBackend.java
+++ src/main/java/rmiage/server/storage/IBackend.java
@@ -3,11 +3,31 @@ package rmiage.server.storage;
 import rmiage.framework.data.IContent;
 
 public interface IBackend {
-	public String add(IContent o);
+	/**
+	 * Store an object.
+	 * @param obj the object to store.
+	 * @return an unique identifier to retrieve the object.
+	 */
+	public String store(IContent obj);
 
+	/**
+	 * Remove an object.
+	 * @param identifier the unique identifier of the object.
+	 */
 	public void remove(String identifier);
 	
-	public IContent get(String identifier);
+	/**
+	 * Load the object identified by the param.
+	 * @param identifier the unique identifier of the object.
+	 * @return
+	 */
+	public IContent load(String identifier);
 	
-	public void update(String identifier, IContent object) throws Exception;
+	/**
+	 * Update an existing object in the backend.
+	 * @param identifier the identifier of the object to update.
+	 * @param object the new version of the object.
+	 * @throws ObjectNotFoundException
+	 */
+	public void update(String identifier, IContent object) throws ObjectNotFoundException;
 }
