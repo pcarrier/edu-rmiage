71dddd9e31266ac4bacdb8550ce112b096f118f4
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-05-04 20:29:20 +0200

    Modification dans le fichier de lancement.

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn package
-java -cp target/rmiage-1.0-SNAPSHOT.jar rmiage.server.controller.MainController --config-file=example-config.properties
+java -cp target/rmiage-1.0-SNAPSHOT.jar rmiage.app.server.MainController --config-file=example-config.properties
