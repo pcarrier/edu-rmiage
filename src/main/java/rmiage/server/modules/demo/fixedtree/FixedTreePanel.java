32fe429b03381c2c636e5603d6b49218c4efb12d
Saad-Dupuy Jean-Christophe <saad.dupuy@yahoo.fr>
2009-05-05 21:48:11 +0200

    Le pannel en question.

---
+++ src/main/java/rmiage/server/modules/demo/fixedtree/FixedTreePanel.java
@@ -0,0 +1,14 @@
+package rmiage.server.modules.demo.fixedtree;
+
+import javax.swing.JPanel;
+
+import rmiage.common.interfaces.Panel;
+
+public class FixedTreePanel extends JPanel implements Panel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4240224812938104012L;
+
+}
